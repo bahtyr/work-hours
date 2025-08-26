a58e371b41a1278a7cce46e26562981c861f4d0a
Ceylan, Ben <ben.ceylan@wada-ama.org>
2025-08-26 02:34:16 -0400

    ui - break time hours

--- src/elements.js
+++ src/elements.js
@@ -8,10 +8,12 @@ export const elements = {
     // hours-summary
     hoursLogged: document.querySelector('.hours-logged .number'),
     hoursLeft: document.querySelector('.hours-left .number'),
+    breakTime: document.querySelector('.break-time .number'),
     ticketsCount: document.querySelector('.tickets-count .number'),
     ticketsCountLabel: document.querySelector('.tickets-count .label'),
     hoursTimeline: document.querySelector('.timeline .done'),
     hoursTimelineHighlight: document.querySelector('.timeline .highlight'),
+    hoursTimelineBreak: document.querySelector('.timeline .break'),
 
     // buttons
     newBtn: document.getElementById('newBtn'),
